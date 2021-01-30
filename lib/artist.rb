7f9c8380d13e3011f80be0417c6d8678968e4588
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-01-30 01:17:47 -0500

    commit tree 2447cfeeadc82144a686870db0dc70efbc592d5a

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,27 @@
+class Artist
+    attr_reader :name
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+    
+    def self.all
+        @@all
+    end
+
+    def new_song(name, genre)
+        new_song = Song.new(name, self, genre)
+        
+    end
+
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end
+
+    def genres
+        songs.map{|song| song.genre}
+    end
+
+end
--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,22 @@
+class Genre
+    attr_accessor :name
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+
+    end
+
+    def songs
+       @songs = Song.all.select {|song| song.genre == self}
+    end
+
+    def artists
+        songs.collect{|song| song.artist}
+    end
+end
